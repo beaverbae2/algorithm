2e8f757f90e3af892ffdaac358ddf849711dd920
beaverbae2 <qogustjr119@naver.com>
2020-11-26 22:11:36 +0900

    test

--- Algo/src/A2020_11_26/Test.java
+++ Algo/src/A2020_11_26/Test.java
@@ -1,7 +1,7 @@
 package A2020_11_26;
 
 public class Test {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 }
