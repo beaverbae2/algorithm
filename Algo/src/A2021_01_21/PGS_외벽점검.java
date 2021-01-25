21747be5d6f648d66dc0e2851581d86b82b21998
beaverbae2 <qogustjr119@naver.com>
2021-01-25 17:31:43 +0900

    solution(Simulation - permutation)

--- Algo/src/A2021_01_21/PGS_외벽점검.java
+++ Algo/src/A2021_01_21/PGS_외벽점검.java
@@ -1,14 +1,17 @@
 package A2021_01_21;
-
-import java.util.*;
-
+/**
+ * 
+ * @author beaverbae
+ * @see ? 
+ *
+ */
 public class PGS_외벽점검 {
 	private int n;
 	private int[] weak;
 	private int[] dist;
 	private int[][] rotateWeak;// rotateWeak[i] : weak[i] 부터 weak[i-1]까지를 표현
 	private boolean isAllChecked;// 모든 외벽이 점검되었는지
-	private int[] selectedDist;
+	private int[] selectedDist;// dist에서 순열로 뽑은 요소
 
 	public int solution(int n, int[] weak, int[] dist) {
 		int answer = -1;
@@ -31,7 +34,8 @@ public class PGS_외벽점검 {
 		
 		return answer;
 	}
-
+	
+	// 순열
 	public void permutation(int r, int n, boolean[] visited) {
 		if (isAllChecked) return;
 		
@@ -51,29 +55,32 @@ public class PGS_외벽점검 {
 
 	}
 	
+	// 외벽 점검
 	public void check() {
-		for (int[] rotate : rotateWeak) {
-			int idx = 0;
-			int cnt = 0;
+		for (int[] rotate : rotateWeak) {// 점검해야 하는 외벽의 경우의 수에 대해
+			int idx = 0;// rotate(외벽 하나)의 인덱스
+			int cnt = 0;// 점검한 외벽의 개수
 			
-			for (int i = 0; i < selectedDist.length; i++) {
+			for (int i = 0; i < selectedDist.length; i++) {// dist에서 뽑은 요소들에 대해
 				int d = selectedDist[i];// 이동거리
 				
-				for (int j = idx; j < rotate.length; j++) {
-					if (rotate[idx] + d >= rotate[j]) {
+				for (int j = idx; j < rotate.length; j++) {// 현재 외벽부터 외벽의 끝까지
+					if (rotate[idx] + d >= rotate[j]) {// 외벽의 현재 위치 + 이동거리 >= 다음 외벽의 위치
 						cnt++;
 					}else break;
 				}
 				
-				idx = cnt;
-				if (cnt == weak.length) isAllChecked = true;
+				idx = cnt;// 다음으로 확인해야할 외벽의 인덱스
+				if (cnt == weak.length) isAllChecked = true;// 정답 찾은 경우
 				
 				if (isAllChecked) break;
 			}
 			if (isAllChecked) break;
 		}
 	}
-
+	
+	
+	// rotateWeak 구현
 	public void setRotateWeak() {
 		int len = weak.length;
 		rotateWeak = new int[len][len];
@@ -82,7 +89,8 @@ public class PGS_외벽점검 {
 			rotateWeak[i] = getRotateWeak(i);
 		}
 	}
-
+	
+	// rotateWeak 구현
 	public int[] getRotateWeak(int idx) {
 		int len = weak.length;
 		int[] result = new int[len];
